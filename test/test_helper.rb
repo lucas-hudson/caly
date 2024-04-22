f18dd37c350179a4d7d7229e280b4a80027c56de
lucas-hudson <lucashudson.contact@gmail.com>
2024-04-22 17:18:08 +0200

    test error type

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -34,12 +34,13 @@ class Minitest::Spec
 
   def self.it_returns_an_error(provider, &block)
     it "must return an Error instance" do
-      json = File.read("test/json/#{provider}/error.json")
+      json = json_response_for(provider, "error")
       stub_request(:any, /.*/).to_return_json(body: json, status: 401)
 
       response = instance_exec(&block)
 
       assert response.is_a?(Caly::Error)
+      assert_equal response.type, "InvalidAuthenticationToken"
       assert_equal response.message, "Request had invalid authentication credentials."
       assert_equal response.code, "401"
     end
